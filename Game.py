7f1fa0a412e019b991517fdfabd4419f78b383ad
MaxEichinger43 <mx.eichinger@gmx.de>
2023-11-14 23:00:54 +0100

    ChatGpt fixed blackscreen

--- Game.py
+++ Game.py
@@ -1,31 +1,26 @@
 import sys
 import math
-#from numpy import arctan
 import pygame
 pygame.init()
 
 debug_mode = True
 font = pygame.font.Font(None, 24)
 
-
-
-#Preparing screen
+# Preparing screen
 screen_width = 1680
 screen_height = 1050
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("RaceTrack")
 
+# Creating surfaces
+Track_surface = pygame.Surface((screen_width, screen_height))
+Racer_surface = pygame.Surface((screen_width, screen_height), pygame.SRCALPHA)
 
-#Creating surfaces
-Track_surface = pygame.Surface((screen_width,screen_height))
-Racer_surface = pygame.Surface((screen_width,screen_height), pygame.SRCALPHA)
-
-
-#Track Graphics loading
+# Track Graphics loading
 track = pygame.image.load("sprites/tracks/track01.png")
 
 class Racer:
-    def __init__ (self, x, y, gfx):
+    def __init__(self, x, y, gfx):
         self.x = x
         self.y = y
         self.gfx = pygame.image.load(gfx)
@@ -46,23 +41,23 @@ class Racer:
         if self.speed < self.max_speed:
             self.x += math.sin(math.radians(self.dir)) * self.speed * delta_time
             self.y -= math.cos(math.radians(self.dir)) * self.speed * delta_time
-        
-        self.y = min(max(self.y, screen_height // 2 - track.get_height // 2), screen_height // 2 + track.get_height // 2)
-        self.x = min(max(self.x, screen_width // 2 - track.get_width // 2), screen_width // 2 + track.get_width // 2)
 
+        self.y = min(max(self.y, screen_height // 2 - track.get_height() // 2), screen_height // 2 + track.get_height() // 2)
+        self.x = min(max(self.x, screen_width // 2 - track.get_width() // 2), screen_width // 2 + track.get_width() // 2)
 
+# Create racer instances
 racer1 = Racer(screen_width // 2, screen_height // 2, "sprites/racers/racer01.png")
 racer2 = Racer(screen_width // 2, screen_height // 2, "sprites/racers/racer02.png")
 
+# Constants
 acceleration = 10
 friction = 0.1
 brake = 20
 
-
-#Creating clock
+# Creating clock
 clock = pygame.time.Clock()
 
-#-------------------------------------------------------------------------------Gameloop
+# Gameloop
 RaceIsRunning = True
 while RaceIsRunning:
     for event in pygame.event.get():
@@ -72,14 +67,12 @@ while RaceIsRunning:
 
     delta_time = clock.tick(120) / 1000.0
 
-
-#Input system...........................
-    
+    # Input system
     keys = pygame.key.get_pressed()
 
-#Racer1
+    # Racer1
     if keys[pygame.K_w]:
-        racer1.speed += acceleration * delta_time // racer1.mass 
+        racer1.speed += acceleration * delta_time // racer1.mass
     else:
         racer1.speed -= friction // racer1.mass
     if keys[pygame.K_a]:
@@ -89,33 +82,29 @@ while RaceIsRunning:
     if keys[pygame.K_d]:
         racer1.dir -= 200 * delta_time
 
-
-
-#Racer2
-if keys[pygame.K_UP]:
-    racer2.speed += acceleration * delta_time // racer2.mass 
-else:
-    racer2.speed -= friction // racer2.mass
-if keys[pygame.K_LEFT]:
-    racer2.dir += 200 * delta_time
-if keys[pygame.K_DOWN]:
-    racer2.speed -= brake // racer2.mass
-if keys[pygame.K_RIGHT]:
-    racer2.dir -= 200 * delta_time 
+    # Racer2
+    if keys[pygame.K_UP]:
+        racer2.speed += acceleration * delta_time // racer2.mass
+    else:
+        racer2.speed -= friction // racer2.mass
+    if keys[pygame.K_LEFT]:
+        racer2.dir += 200 * delta_time
+    if keys[pygame.K_DOWN]:
+        racer2.speed -= brake // racer2.mass
+    if keys[pygame.K_RIGHT]:
+        racer2.dir -= 200 * delta_time
 
     racer1.move()
     racer2.move()
-
 
     if debug_mode:
         debug_text = f"Racer1 Speed: {racer1.speed:.2f}, Position: ({racer1.x:.2f}, {racer1.y:.2f}), Keys pressed: {keys}"
         debug_surface = font.render(debug_text, True, (255, 255, 255))
         screen.blit(debug_surface, (10, 10))
 
-
-    #Drawing the track and the clear racer surface
-    Track_surface.fill((255,255,255))
-    Track_surface.blit(track, (screen_width // 2 - track.get_width // 2,screen_height // 2 - track.get_height // 2))
+    # Drawing the track and the clear racer surface
+    Track_surface.fill((255, 255, 255))
+    Track_surface.blit(track, (screen_width // 2 - track.get_width() // 2, screen_height // 2 - track.get_height() // 2))
     Racer_surface.fill((0, 0, 0, 0))
 
     racer1.update_n_draw()
@@ -125,8 +114,6 @@ if keys[pygame.K_RIGHT]:
     screen.blit(Track_surface, (0, 0))
     screen.blit(Racer_surface, (0, 0))
 
-
-    
     pygame.display.flip()
     pygame.display.update()
     clock.tick(120)
